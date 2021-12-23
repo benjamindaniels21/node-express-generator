7b308cb7781fc0dadd1c042160678e2cc7301964
Ben Daniels <benjamindaniels21@gmail.com>
2021-12-23 18:24:16 -0500

    mongoose error handling

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,27 +1,40 @@
 const express = require("express");
+const Campsite = require("../models/campsite");
+
 const campsiteRouter = express.Router();
 
 campsiteRouter
   .route("/")
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    next();
+  .get((req, res, next) => {
+    Campsite.find()
+      .then((campsites) => {
+        require.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(campsites);
+      })
+      .catch((err) => next(err));
   })
-  .get((req, res) => {
-    res.end("Will send all the campsites to you");
-  })
-  .post((req, res) => {
-    res.end(
-      `Will add the campsite: ${req.body.name} with description: ${req.body.description}`
-    );
+  .post((req, res, next) => {
+    Campsite.create(req.body)
+      .then((campsite) => {
+        console.log("Campsite Created ", campsite);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(campsite);
+      })
+      .catch((err) => next(err));
   })
   .put((req, res) => {
     res.statusCode = 403;
     res.end("PUT operation not supported on /campsites");
   })
-  .delete((req, res) => {
-    res.end("Deleting all campsites");
+  .delete((req, res, next) => {
+    Campsite.deleteMany()
+      .then((response) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+      })
+      .catch((err) => next(err));
   });
 
 campsiteRouter
